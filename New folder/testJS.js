3e8c8e250eb3e52533f232df7f8b9f05ad17170e
klynn726 <49536087+klynn726@users.noreply.github.com>
2021-03-27 10:28:11 -0700

    working date picker

---
+++ New folder/testJS.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.datepicker');
+  var instances = M.Datepicker.init(elems, options);
+});
+
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.timepicker');
+  var instances = M.Timepicker.init(elems, options);
+});
+
+
+
