211f984d882a4e8fc358f79c49a96853e52df8c7
Vicky Loebel <vickyloebel@gmail.com>
2021-03-27 15:18:36 -0700

    participant routes ready for test and merge

--- controllers/api/participant-routes.js
+++ controllers/api/participant-routes.js
@@ -5,20 +5,37 @@ const withAuth = require('../../utils/auth');
 
 ////////////////////////////////////////
 //  THESE ARE THE PARTICIPANT ROUTES
-//  (P0) Get the table without includes
-//  (P1) GET all the meetings for a specified user 
-//       /api/participants/meeting/:user_id
-//  (P2) GET all the users for a specified meeting
+//  --  This includes test queries for insomnia
+//  --  because they can be a bit confusing
+//  --  note that you might have to delete before inserting
+//  --  if something's been added already
+//  (P0) Dump the participant table (without includes)
+//       GET localhost:3002/api/participants
+//  (P1) Find all the meetings for a specified user 
+//       /api/participants/meetings/:user_id
+//       for example, all the meetings where user_id=6
+//       GET localhost:3002/api/participants/meetings/6
+//  (P2) Find all the users for a specified meeting
 //      /api/participants/users/:meeting_id
-//  (P3) ADD a participant and  meeting pair to Participant table
-//  (P4) DELETE a participant from a meeting
+//       for example, all the meetings where meeting_id=3
+//       GET localhost:3002/api/participants/users/3
+//  (P3) Add a participant and  meeting pair to Participant table
+//       (don't worry about setting accepted flag - user 
+//       will do that later)
+//       POST localhost:3002/api/participants/
+//       { "meeting_id": "6",  "user_id": "6" }
+//  (P4) Delete a participant from a meeting
+//       Delete api/participants/?user=id&meeting=id, for example
+//       DELETE localhost:3002/api/participants/?user=4&meeting=4
 //  (P5) UPDATE the Accepted flag for a Participant
-//       put /api/participants/  
-// user_id: req.body.user_id,
-// meeting_id: req.body.meeting_id,
-// accepted:req.body.accepted
+//       PUT api/participants/?user=id&meeting=id
+//       with {accepted:boolean} in the body, for example:
+//       PUT localhost:3002/api/participants/?user=4&meeting=4
+//        { "accepted":"true"}
 //////////////////////////////////////////////////
-// GET ALL user/meeting entries from Participants
+
+//////////////////////////////////////////////////
+// (P0) Dump the Participant table
 // Might be useful for an admin or something?
 
 router.get('/', (req, res) => {
@@ -40,7 +57,6 @@ router.get('/', (req, res) => {
       res.status(500).json(err);
     });
 });
-
 
 /////////////////////////////////////////
 //  (P1) Get all meetings for specified user
@@ -80,7 +96,7 @@ router.get('/meetings/:id', (req, res) => {
 });
 
 /////////////////////////////////////////
-//  (P2) Get all the users for a specified meeting_id
+//  (P2) Get all the users invited to a specified meeting_id
 //      /api/participants/users/:meeting_id
 
 router.get('/users/:id', (req, res) => {
@@ -118,9 +134,10 @@ router.get('/users/:id', (req, res) => {
 
 
 /////////////////////////////////////////////////////////
-//  (P3) ADD A PARTICIPANT AND MEETING pair
+//  (P3) CREATE A meeting/user entry
 //  post /api/participants/
-//  leave "accepted" flag null for user to later accept or decline
+//  leave "accepted" flag null because the person who
+//  invites doesn't know if they will accept or not
 
 router.post('/', (req, res) => {
   Participant.create({
@@ -137,18 +154,16 @@ router.post('/', (req, res) => {
 });
 
 //  (P4) DELETE a user/meeting pair, effectively
-//  removing that user from the meeting
+//  removing that user's invitation from the meeting
 // there could be other routes, like remove ALL
 // of a user's meetings, but that' not in the spec. 
 // DELETE api/participants  - user id and meeting id in body.
 
-// only question - is this an and?
-
 router.delete('/', (req, res) => {
-  Participant.destroy({
+  Participant.destroy( {
     where: {
-      user_id: req.body.user_id,
-      meeting_id: req.body.meeting_id
+      user_id: req.query.user,
+      meeting_id: req.query.meeting
     }
   })
     .then(dbParticipantDataa => {
@@ -169,6 +184,8 @@ router.delete('/', (req, res) => {
 //  Update "accepted" flag
 //  
 //  put api/participants/?user=id&meeting=id
+//   and then have the flag in the body:
+//   { accepted:true}
 
 
 router.put('/', (req, res) => {
