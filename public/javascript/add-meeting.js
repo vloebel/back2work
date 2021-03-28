175480dbe350c9b5e14269214fa716fe27b12796
Vicky Loebel <vickyloebel@gmail.com>
2021-03-28 15:34:45 -0700

    commented document flow for add-meeting code created in group

--- public/javascript/add-meeting.js
+++ public/javascript/add-meeting.js
@@ -1,11 +1,34 @@
-async function newFormHandler(event) {
+//////////////////////////////////////////////
+//  add-meeting.js
+//////////////////////////////////////////////
+//
+// CONTROL FLOW: 
+// dashboard-routes ->
+//   dashboard.handlebars ->
+//    add-meeting.handlebars ->
+//      add-meeting.js
+//////////////////////////////////////////////
+
+const { Meeting } = require("../../models");
+
+
+//////////////////////////////////////////////
+// Places an event listner on new-meeting-form SUBMIT BUTTON
+//  on SUBMIT addMeetingHandler 
+//  LOADS data from the add-meeting.handlebars form and
+//  POSTS the info to the route: /api/meetings 
+//  if response  ok RETURNS via 
+//  document.location.replace('/dashboard');//
+
+async function addMeetingHandler(event) {
     event.preventDefault();
     const title = document.querySelector('input[name="meeting-title"]').value.trim();
-    const topic = document.querySelector('textarea[name="meeting-topic"]').value.trim();
-    const date = 2021 / 03 / 28
-    const start = 9
-    const end = 10
-    const organizer = req.session.user_id
+  const topic = document.querySelector('textarea[name="meeting-topic"]').value.trim();
+  // hard code some test data
+  const date = '2021/03/28';
+  const start = 9;
+  const end = 10;
+  const organizer = req.session.user_id;
     const response = await fetch(`/api/meetings`, {
         method: 'POST',
         body: JSON.stringify({
@@ -26,5 +49,7 @@ async function newFormHandler(event) {
         alert(response.statusText);
     }
 }
-// Submit a post and then return to dashboard
-document.querySelector('.new-meeting-form').addEventListener('submit', newFormHandler);
+/////////////////////////////////////////////////////
+// event listener 
+/////////////////////////////////////////////////////
+document.querySelector('.add-meeting-form').addEventListener('submit', addMeetingHandler);
