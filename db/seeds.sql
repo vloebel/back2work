e26c6846ba701041d0101abc4bab624609e04b5e
Vicky Loebel <vickyloebel@gmail.com>
2021-03-29 14:58:12 -0700

    updated seeds file to make start into a string

--- db/seeds.sql
+++ db/seeds.sql
@@ -10,16 +10,16 @@ insert into user (firstname, lastname, email, password, available) values ('Rikk
 insert into user (firstname, lastname, email, password, available) values ('Ninnette', 'Perkins', 'nperkins8@mail.ru', 'HNb02BhoTgh7', null);
 insert into user (firstname, lastname, email, password, available) values ('Clayson', 'Farley', 'cfarley9@csmonitor.com', 'JDGh6TC', 'TR');
 
-insert into meeting (date, start, end, meeting_name, topic, organizer_id) values ('2020/08/14', 11, null, 'Coffee', 'Morbi non , venenatis non, sodales sed, tincidunt eu, felis.', 9);
-insert into meeting (date, start, end, meeting_name, topic, organizer_id) values ('2021/03/18', 11, null, 'Document Security', 'Aliquam  tortor quis turpis.', 7);
-insert into meeting (date, start, end, meeting_name, topic, organizer_id) values ('2020/06/30', 9, 16, 'Man from U.N.C.L.E.', 'In sag pretium quis, lectus.', 4);
-insert into meeting (date, start, end, meeting_name, topic, organizer_id) values ('2020/03/28', 11, 17, 'Database Design', 'Integer tido placerat.', 8);
-insert into meeting (date, start, end, meeting_name, topic, organizer_id) values ('2021/01/21', 12, 16, 'More Database Design', 'Nullam sit m. Integer a nibh.', 4);
-insert into meeting (date, start, end, meeting_name, topic, organizer_id) values ('2020/05/09', 9, 17, 'parking etiquette', 'Praenean sit amet justo. Morbi ut odio.', 5);
-insert into meeting (date, start, end, meeting_name, topic, organizer_id) values ('2020/07/18', 9, 15, 'employee announcements', 'Quisque is et ultrices pojusto, sollicitudin ut, suscipit a, feugiat et, eros.', 8);
-insert into meeting (date, start, end, meeting_name, topic, organizer_id) values ('2021/02/14', 12, 14, 'Coffee', 'Morbi porue in, tempus sit amet, sem.', 6);
-insert into meeting (date, start, end, meeting_name, topic, organizer_id) values ('2020/08/03', 12, 16, 'annual meeting', 'Curabiicitudin vitae, consectetuer eget, rutrum at, lorem.', 6);
-insert into meeting (date, start, end, meeting_name, topic, organizer_id) values ('2021/03/21', 10, 14, 'Conservation', 'Phasellus in flibero. Nam dui.', 7);
+insert into meeting (date, start, duration, meeting_name, topic, organizer_id) values ('2020/08/14', '09:00', null, 'Coffee', 'Morbi non , venenatis non, sodales sed, tincidunt eu, felis.', 9);
+insert into meeting (date, start, duration, meeting_name, topic, organizer_id) values ('2021/03/18', '10:00', null, 'Document Security', 'Aliquam  tortor quis turpis.', 7);
+insert into meeting (date, start, duration, meeting_name, topic, organizer_id) values ('2020/06/30', '11:00', 1.5, 'Man from U.N.C.L.E.', 'In sag pretium quis, lectus.', 4);
+insert into meeting (date, start, duration, meeting_name, topic, organizer_id) values ('2020/03/28', '12:00', 1, 'Database Design', 'Integer tido placerat.', 8);
+insert into meeting (date, start, duration, meeting_name, topic, organizer_id) values ('2021/01/21', '13:00', 1.5, 'More Database Design', 'Nullam sit m. Integer a nibh.', 4);
+insert into meeting (date, start, duration, meeting_name, topic, organizer_id) values ('2020/05/09', '14:30', 1, 'parking etiquette', 'Praenean sit amet justo. Morbi ut odio.', 5);
+insert into meeting (date, start, duration, meeting_name, topic, organizer_id) values ('2020/07/18', '14:00', 1, 'employee announcements', 'Quisque is et ultrices pojusto, sollicitudin ut, suscipit a, feugiat et, eros.', 8);
+insert into meeting (date, start, duration, meeting_name, topic, organizer_id) values ('2021/02/14', '09:30', 1.5, 'Coffee', 'Morbi porue in, tempus sit amet, sem.', 6);
+insert into meeting (date, start, duration, meeting_name, topic, organizer_id) values ('2020/08/03', '15:00', 1, 'annual meeting', 'Curabiicitudin vitae, consectetuer eget, rutrum at, lorem.', 6);
+insert into meeting (date, start, duration, meeting_name, topic, organizer_id) values ('2021/03/21', '15:30', 1, 'Conservation', 'Phasellus in flibero. Nam dui.', 7);
 
 insert into participant (user_id, meeting_id, accepted) values ('1','1', true);
 insert into participant (user_id, meeting_id, accepted) values ('5','1', false);
@@ -29,5 +29,5 @@ insert into participant (user_id, meeting_id, accepted) values ('8','3', true);
 insert into participant (user_id, meeting_id, accepted) values ('6','3', false);
 insert into participant (user_id, meeting_id, accepted) values ('6','4', null);
 insert into participant (user_id, meeting_id, accepted) values ('4','4', false);
-insert into participant (user_id, meeting_id, accepted) values ('4','5', true);
+
 
