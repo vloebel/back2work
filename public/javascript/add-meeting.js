240942dc6bc6aaeac5fa2b7c600193acd9810fde
Vicky Loebel <vickyloebel@gmail.com>
2021-03-29 12:44:14 -0700

    putting in kates datepicker by hand

--- public/javascript/add-meeting.js
+++ public/javascript/add-meeting.js
@@ -29,33 +29,52 @@ async function addMeetingHandler(event) {
   const date = '2021/03/28';
   const start = 9;
   const end = 10;
-  
+
   // The names below must match the database column names
   // the id of the organizer is the logged-in user's id
   // it is set to req.session.user_id in the route
 
-    const response = await fetch(`/api/meetings`, {
-        method: 'POST',
-        body: JSON.stringify({
-            date,
-            start,
-            end,
-            meeting_name,
-            topic
-        }),
-        headers: {
-            'Content-Type': 'application/json'
-        }
-    });
+  const response = await fetch(`/api/meetings`, {
+    method: 'POST',
+    body: JSON.stringify({
+      date,
+      start,
+      end,
+      meeting_name,
+      topic
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
   if (response.ok) {
     console.log(`///////////////////////////`)
     console.log(`///  response OK  /////`)
     console.log(`///////////////////////////`)
     document.location.replace('/dashboard');
-    } else {
-        alert(response.statusText);
-    }
+  } else {
+    alert(response.statusText);
+  }
 }
+
+/////////////////////////////////////////////////
+//   date picker 
+///////////////////////////////////////////////
+
+$(function () {
+  $('#dateTime').ejDateTimePicker({
+    interval: 60,
+    dateTimeFormat: "M/d/y HH:mm",
+    width: '180px',
+    value: new Date(),
+    timeDrillDown: {
+      enabled: true,
+      interval: 30,
+      showMeridian: false
+    },
+  });
+});
+
 /////////////////////////////////////////////////////
 // event listener 
 /////////////////////////////////////////////////////
